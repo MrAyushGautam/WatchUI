ad3067e6dc3ce3a1c60fd256869ccc8621697f3a
MrAyushGautam <info.ayushgautam@gmail.com>
2024-09-08 15:42:11 +0530

    Initial Commit

---
+++ WatchUI/WatchUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchUIApp.swift
+//  WatchUI
+//
+//  Created by Morning Star on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
